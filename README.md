865e928a1f779c7857827c67d8ade8df50234b3d
Courtney828 <courtneyserongwa@gmail.com>
2020-08-18 09:02:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-lpq5fb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-lpq5fb)
---
+++ index.ts
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let ToDoList = [{TaskName:"challange1", TaskDate:"3/August/2020",TaskStatus:"done"},
+                {TaskName:"task1", TaskDate:"10/August/2020", TaskStatus:"done"},
+                {TaskName:"challange2", TaskDate:"17/August/2020", TaskStatus:"dodo"},
+                {TaskName:"task2", TaskDate:"24/August/2020", TaskStatus:"todo"},
+                {TaskName:"challange3", TaskDate:"31/August/2020", TaskStatus:"todo"},
+                {TaskName:"task3", TaskDate:"7/September/2020", TaskStatus:"todo"},
+                {TaskName:"challange4", TaskDate:"14/September/2020", TaskStatus:"todo"},
+                {TaskName:"task4", TaskDate:"21/September/2020", TaskStatus:"todo"},
+                {TaskName:"challange5", TaskDate:"28/September/2020", TaskStatus:"todo"},
+                {TaskName:"task5", TaskDate:"5/October/2020",TaskStatus:"todo"}]
+
+
+console.log(ToDoList[4]);               
